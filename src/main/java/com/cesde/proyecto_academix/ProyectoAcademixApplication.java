ebef2218a2f1deace6395a960d24c2e54d1df865
Juan kevin betancur sierra <jbetancurs@cesde.net>
2026-08-23 15:32:12 -0500

    chore: proyecto base generado con Spring Initializr (Boot 4.1.0)

---
+++ src/main/java/com/cesde/proyecto_academix/ProyectoAcademixApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.proyecto_academix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAcademixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAcademixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesde/proyecto_academix/ProyectoAcademixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.proyecto_academix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAcademixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
