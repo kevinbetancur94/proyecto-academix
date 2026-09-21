38a2e6bbe8fb6940e31f593e444d21e5391ed064
nquintero <natycesde2025@gmail.com>
2026-09-20 21:24:15 -0500

    feat(calificacion): agregue repository, service y controller de Calificacion

---
+++ src/main/java/com/cesde/proyecto_academix/repository/CalificacionRepository.java
@@ -0,0 +1,8 @@
+package com.cesde.proyecto_academix.repository;
+
+import com.cesde.proyecto_academix.model.entity.Calificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CalificacionRepository extends JpaRepository<Calificacion, Long> {
+}
